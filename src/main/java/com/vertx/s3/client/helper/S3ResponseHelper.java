d10a7d9b7cb5aa8457016d7a9f743ee651d4ae7e
bruno <bruno_riscado@yahoo.com>
2015-11-03 21:30:55 +0000

    addind http response handler

--- src/main/java/com/vertx/s3/client/helper/S3ResponseHelper.java
+++ src/main/java/com/vertx/s3/client/helper/S3ResponseHelper.java
@@ -1,8 +1,13 @@
 package com.vertx.s3.client.helper;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.multipart.HttpPostRequestDecoder;
 import io.netty.util.internal.StringUtil;
+import io.vertx.rxjava.core.http.HttpClientResponse;
+import rx.Observable;
+
+import java.io.IOException;
 
 import static com.jayway.restassured.path.xml.XmlPath.with;
 
@@ -10,13 +15,36 @@ import static com.jayway.restassured.path.xml.XmlPath.with;
  * Created by bruno on 28/10/15.
  */
 public class S3ResponseHelper {
+    private XmlMapper xmlMapper;
+
+    private S3ResponseHelper() {
+        xmlMapper = new XmlMapper();
+    }
+
+    public static S3ResponseHelper getIntance() {
+        return InternalS3ResponseHelper.INSTANCE;
+    }
+
     public String getUploadIdFromResponse(String response) {
         return with(response).get("InitiateMultipartUploadResult.UploadId");
     }
 
+    public <T> Observable<T> getEntityFromResponse(HttpClientResponse response, Class<T> type) {
+        return response.toObservable()
+                .collect(() -> new StringBuffer(),
+                        (sb, buffer) -> sb.append(buffer))
+                .flatMap(sBuffer -> {
+                    try {
+                        return Observable.just(xmlMapper.readValue(sBuffer.toString(), type));
+                    } catch (IOException e) {
+                        return Observable.error(e);
+                    }
+                });
+    }
+
     // Taken From Netty's implementation which is protected
     // io.netty.handler.codec.http.multipart.HttpPostMultipartRequestDecoder;
-    public static String[] getMultipartDataBoundary(String contentType) {
+    public String[] getMultipartDataBoundary(String contentType) {
         // Check if Post using "multipart/form-data; boundary=--89421926422648 [; charset=xxx]"
         String[] headerContentType = splitHeaderContentType(contentType);
         if (headerContentType[0].toLowerCase().startsWith(
@@ -49,15 +77,15 @@ public class S3ResponseHelper {
                     HttpHeaders.Values.CHARSET)) {
                 String charset = StringUtil.substringAfter(headerContentType[crank], '=');
                 if (charset != null) {
-                    return new String[] {"--" + boundary, charset};
+                    return new String[]{"--" + boundary, charset};
                 }
             }
-            return new String[] {"--" + boundary};
+            return new String[]{"--" + boundary};
         }
         return null;
     }
 
-    private static String[] splitHeaderContentType(String sb) {
+    private String[] splitHeaderContentType(String sb) {
         int aStart;
         int aEnd;
         int bStart;
@@ -65,34 +93,35 @@ public class S3ResponseHelper {
         int cStart;
         int cEnd;
         aStart = findNonWhitespace(sb, 0);
-        aEnd =  sb.indexOf(';');
+        aEnd = sb.indexOf(';');
         if (aEnd == -1) {
-            return new String[] { sb, "", "" };
+            return new String[]{sb, "", ""};
         }
         bStart = findNonWhitespace(sb, aEnd + 1);
         if (sb.charAt(aEnd - 1) == ' ') {
             aEnd--;
         }
-        bEnd =  sb.indexOf(';', bStart);
+        bEnd = sb.indexOf(';', bStart);
         if (bEnd == -1) {
             bEnd = findEndOfString(sb);
-            return new String[] { sb.substring(aStart, aEnd), sb.substring(bStart, bEnd), "" };
+            return new String[]{sb.substring(aStart, aEnd), sb.substring(bStart, bEnd), ""};
         }
         cStart = findNonWhitespace(sb, bEnd + 1);
         if (sb.charAt(bEnd - 1) == ' ') {
             bEnd--;
         }
         cEnd = findEndOfString(sb);
-        return new String[] { sb.substring(aStart, aEnd), sb.substring(bStart, bEnd), sb.substring(cStart, cEnd) };
+        return new String[]{sb.substring(aStart, aEnd), sb.substring(bStart, bEnd), sb.substring(cStart, cEnd)};
     }
 
     /**
      * Find the first non whitespace
+     *
      * @return the rank of the first non whitespace
      */
-    static int findNonWhitespace(String sb, int offset) {
+    int findNonWhitespace(String sb, int offset) {
         int result;
-        for (result = offset; result < sb.length(); result ++) {
+        for (result = offset; result < sb.length(); result++) {
             if (!Character.isWhitespace(sb.charAt(result))) {
                 break;
             }
@@ -102,11 +131,12 @@ public class S3ResponseHelper {
 
     /**
      * Find the first whitespace
+     *
      * @return the rank of the first whitespace
      */
-    static int findWhitespace(String sb, int offset) {
+    int findWhitespace(String sb, int offset) {
         int result;
-        for (result = offset; result < sb.length(); result ++) {
+        for (result = offset; result < sb.length(); result++) {
             if (Character.isWhitespace(sb.charAt(result))) {
                 break;
             }
@@ -116,15 +146,20 @@ public class S3ResponseHelper {
 
     /**
      * Find the end of String
+     *
      * @return the rank of the end of string
      */
-    static int findEndOfString(String sb) {
+    int findEndOfString(String sb) {
         int result;
-        for (result = sb.length(); result > 0; result --) {
+        for (result = sb.length(); result > 0; result--) {
             if (!Character.isWhitespace(sb.charAt(result - 1))) {
                 break;
             }
         }
         return result;
+    }
+
+    private static class InternalS3ResponseHelper {
+        private static final S3ResponseHelper INSTANCE = new S3ResponseHelper();
     }
 }
