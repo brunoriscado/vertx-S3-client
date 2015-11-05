abdc12dce5fa77cd6ee06f4a0b8f2c5f2e263c5a
bruno <bvilhena@northplains.com>
2015-11-05 17:10:07 +0000

    working version of inputstream readstream wrapper

--- src/test/java/com/vertx/s3/client/unit/ReadInputStream.java
+++ src/test/java/com/vertx/s3/client/unit/ReadInputStream.java
@@ -1,13 +1,12 @@
 package com.vertx.s3.client.unit;
 
-import io.vertx.rxjava.core.Vertx;
 import io.vertx.rxjava.core.buffer.Buffer;
 import io.vertx.rxjava.core.streams.ReadStream;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -15,56 +14,35 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * Created by bruno on 04-11-2015.
  */
 public class ReadInputStream extends InputStream {
-    private Vertx vertx;
     private AtomicBoolean readStreamFinished;
     private AtomicBoolean readStreamPaused;
-//    private static final int MAX_QUEUE = 32768;
-private static final int MAX_QUEUE = 1000;
-    private BlockingQueue<Byte> queue;
+    private static final int MAX_QUEUE = 32768;
+    private BlockingDeque<Byte> activeQ;
+    private BlockingDeque<Byte> stoppedBuffer;
     private ReadStream<Buffer> inputStream;
-    private Buffer vertxBuffer;
-    private long counter = 0;
-    private int bufCounter = 0;
 
-    public static ReadInputStream getInstance(Vertx vertx, ReadStream<Buffer> inputStream) {
-        return InnderReadInputStream.getInstance(vertx, inputStream);
+    public static ReadInputStream getInstance(ReadStream<Buffer> inputStream) {
+        return InnderReadInputStream.getInstance(inputStream);
     }
 
-    private ReadInputStream(Vertx vertx, ReadStream<Buffer> inputStream) {
-        this.vertx = vertx;
-        this.vertxBuffer = Buffer.buffer();
+    private ReadInputStream(ReadStream<Buffer> inputStream) {
         this.readStreamPaused = new AtomicBoolean(true);
         this.readStreamFinished = new AtomicBoolean(false);
-        this.queue = new LinkedBlockingQueue<Byte>(MAX_QUEUE);
+        this.activeQ = new LinkedBlockingDeque<Byte>(MAX_QUEUE);
+        this.stoppedBuffer = new LinkedBlockingDeque<Byte>();
         this.inputStream = inputStream;
-        stop();
 
-        /**
-         * for each buffer chunk handled check if the entire chunk will fit in the queue
-         * if it doesn't stop the stream and save the chunk in memory temporarily so that read()
-         * can process it when it runs out of queue entries and resume the stream
-         *
-         * TODO - change this to check queue size on each byte
-         */
         this.inputStream.handler(handleBuffer -> {
-//            if (queue.remainingCapacity() >= handleBuffer.length()) {
-                bufCounter = handleBuffer.length();
-                while (bufCounter > 0) {
-                    bufCounter--;
-                    counter++;
-
-                    //This Should never happen because we are checking the full chunk will fit the queue
-                    if (queue.size() == MAX_QUEUE) {
+                int index = 0;
+                while (index < handleBuffer.length()) {
+                    if (activeQ.remainingCapacity() == 0) {
+                        stoppedBuffer.offerFirst(handleBuffer.getByte(index));
                         stop();
-                        vertxBuffer.appendByte(handleBuffer.getByte(bufCounter));
                     } else {
-                        queue.offer(handleBuffer.getByte(bufCounter));
+                        activeQ.offerFirst(handleBuffer.getByte(index));
                     }
+                    index++;
                 }
-//            } else {
-//                stop();
-//                vertxBuffer = handleBuffer;
-//            }
         });
 
         this.inputStream.endHandler(endHandle -> {
@@ -84,107 +62,73 @@ private static final int MAX_QUEUE = 1000;
         return this;
     }
 
+    private int readInt() throws IOException, InterruptedException {
+        Byte b = null;
+        if (readStreamFinished.get()) {
+            //Stream finished check if activeQ and pausedBuffer still have bytes
+            if (stoppedBuffer.isEmpty() && activeQ.isEmpty()) {
+                //if activeQ and stopped buffer are also empty finish by return -1
+                b = null;
+            } else {
+                if (stoppedBuffer.isEmpty()) {
+                    //activeQ still has bytes
+                    b = activeQ.pollLast(5000, TimeUnit.MILLISECONDS);
+                } else {
+                    //Stopped buffer still has bytes
+                    b = stoppedBuffer.pollLast();
+                }
+            }
+        } else {
+            //Stream hasn't finished yet
+            if (readStreamPaused.get()) {
+                if (activeQ.isEmpty()) {
+                    //activeQ is empty, start draining stoppedBuffer
+                    if (stoppedBuffer.isEmpty()) {
+                        //stoppedBuffer is drained, restart stream
+                        start();
+                        b = activeQ.takeLast();
+                    } else {
+                        //stoppedBuffer contains bytes, poll it
+                        b = stoppedBuffer.pollLast();
+                    }
+                } else {
+                    b = activeQ.pollLast(5000, TimeUnit.MILLISECONDS);
+                }
+            } else {
+                if (activeQ.isEmpty()) {
+                    //activeQ is empty, start draining stoppedBuffer
+                    if (stoppedBuffer.isEmpty()) {
+                        //stoppedBuffer is drained, restart stream
+                        start();
+                        b = activeQ.takeLast();
+                    } else {
+                        //stoppedBuffer contains bytes, poll it
+                        b = stoppedBuffer.pollLast();
+                    }
+                } else {
+                    b = activeQ.pollLast(5000, TimeUnit.MILLISECONDS);
+                }
+            }
+        }
+        return b == null ? -1 : Byte.toUnsignedInt(b);
+    }
+
     //Stream overrides to copy the data
     @Override
     public int read() throws IOException {
-        Byte b = null;
+        int result = 0;
         try {
-            if (!readStreamFinished.get()) {
-                if (!queue.isEmpty()) {
-                    //Not finished and queue not empty - poll queue
-                    b = queue.poll(5000, TimeUnit.MILLISECONDS);
-                } else {
-                    //not finished and queue empty copy buffer to queue, reset buffer and restart stream
-                    if (vertxBuffer != null) {
-                        if (vertxBuffer.length() <= MAX_QUEUE) {
-                            for (int i = 0; i <= vertxBuffer.length(); i++) {
-                                queue.offer(vertxBuffer.getByte(i));
-                            }
-                        } else {
-                            for (int i = 0; i <= MAX_QUEUE; i++) {
-                                queue.offer(vertxBuffer.getByte(i));
-                            }
-                            Buffer temp = Buffer.buffer();
-                            for (int i = MAX_QUEUE; i <= vertxBuffer.length(); i++) {
-                                temp.appendByte(vertxBuffer.getByte(i));
-                            }
-                            vertxBuffer = temp;
-                        }
-                        if (readStreamPaused.get()) {
-                            start();
-                        }
-                    }
-                    b = queue.poll(5000, TimeUnit.MILLISECONDS);
-                }
-            } else {
-                if (!queue.isEmpty()) {
-                    //finished and queue not empty - poll queue
-                    b = queue.poll(5000, TimeUnit.MILLISECONDS);
-                } else {
-                    //finished and queue empty copy buffer to queue, reset buffer and restart stream
-                    if (vertxBuffer != null) {
-                        if (vertxBuffer.length() <= MAX_QUEUE) {
-                            for (int i = 0; i <= vertxBuffer.length(); i++) {
-                                queue.offer(vertxBuffer.getByte(i));
-                            }
-                        } else {
-                            for (int i = 0; i <= MAX_QUEUE; i++) {
-                                queue.offer(vertxBuffer.getByte(i));
-                            }
-                            Buffer temp = Buffer.buffer();
-                            for (int i = MAX_QUEUE; i <= vertxBuffer.length(); i++) {
-                                temp.appendByte(vertxBuffer.getByte(i));
-                            }
-                            vertxBuffer = temp;
-                        }
-                        if (readStreamPaused.get()) {
-                            start();
-                        }
-                    }
-                    b = queue.poll(5000, TimeUnit.MILLISECONDS);
-                }
-            }
-
+            result = readInt();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return b == null ? -1 : b.intValue();
+        return result;
     }
 
     @Override
-    public int read(byte[] b) throws IOException {
-        return read(b, 0, b.length);
-    }
-
-    @Override
-    public int read(byte[] b, int off, int len) throws IOException {
-        int read = off;
-        if (b == null) {
-            throw new NullPointerException();
-        } else if (off < 0 || len < 0 || len > b.length - off) {
-            throw new IndexOutOfBoundsException();
-        } else if (len == 0) {
-            return 0;
-        }
-
-        int c = read();
-        if (c == -1) {
-            return -1;
-        }
-        b[off] = (byte)c;
-
-        int i = 1;
-
-        for (; i < len ; i++) {
-            c = read();
-            if (c == -1) {
-                break;
-            }
-            b[off + i] = (byte)c;
-            read += i;
-        }
-
-        return i;
+    public void close() throws IOException {
+        stop();
+        super.close();
     }
 
     @Override
@@ -192,18 +136,12 @@ private static final int MAX_QUEUE = 1000;
         return 0;
     }
 
-    @Override
-    public void close() throws IOException {
-        stop();
-        this.close();
-    }
-
     private static class InnderReadInputStream {
         static ReadInputStream INSTANCE;
 
-        public static ReadInputStream getInstance(Vertx vertx, ReadStream<Buffer> inputstream) {
+        public static ReadInputStream getInstance(ReadStream<Buffer> inputstream) {
             if (INSTANCE == null) {
-                INSTANCE = new ReadInputStream(vertx, inputstream);
+                INSTANCE = new ReadInputStream(inputstream);
             }
             return INSTANCE;
         }
--- src/test/java/com/vertx/s3/client/unit/TestReadInputStream.java
+++ src/test/java/com/vertx/s3/client/unit/TestReadInputStream.java
@@ -6,14 +6,12 @@ import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import io.vertx.rxjava.core.Vertx;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import rx.Observable;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 /**
  * Created by bruno on 04-11-2015.
@@ -28,24 +26,28 @@ public class TestReadInputStream {
     }
 
     @Test
+//    @Ignore
     public void testInputStreamWrapper(TestContext context) {
         Async async = context.async();
         vertx.fileSystem().openObservable("static/test2.png", new OpenOptions().setWrite(true).setRead(true))
                 .flatMap(asyncFile -> {
-                    InputStream in = ReadInputStream.getInstance(vertx, asyncFile).start();
+                    InputStream in = ReadInputStream.getInstance(asyncFile);//.start();
                     FileOutputStream fileStr = null;
+
                     try {
+                        int count = 0;
 
                         fileStr = new FileOutputStream(new File("/tmp/testFile.png"));
                         int read = 0;
-                        byte[] bytes = new byte[1024];
+                        byte[] bytes = new byte[1];
 
                         while ((read = in.read(bytes)) != -1) {
+                            count++;
                             fileStr.write(bytes, 0, read);
                         }
 
                         System.out.println("Done!");
-                        return Observable.just(new File("/tmp/testFile.png"));
+                        return Observable.just(null);
                     } catch (IOException e) {
                         return Observable.error(e);
                     } finally {
@@ -55,6 +57,7 @@ public class TestReadInputStream {
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
+
                         }
                         if (fileStr != null) {
                             try {
@@ -67,10 +70,88 @@ public class TestReadInputStream {
                         }
                     }
                 })
-                .subscribe(file -> {
+                            .subscribe(v -> {
+                    File file = new File("/tmp/testFile.png");
                     context.assertTrue(file.exists());
-                    async.complete();
-                },
-                context::fail);
+                                        async.complete();
+                                    },
+                                    context::fail);
+                }
+
+
+    @Test
+    @Ignore
+    public void testInputStreamWrapperFileStream(TestContext context) {
+        Async async = context.async();
+        try {
+            FileInputStream in = new FileInputStream(new File("/home/bruno/workspace/vertx-S3-client/src/main/resources/static/test.txt"));
+            int count = 0;
+            int read = 0;
+
+            while ((read = in.read()) != -1) {
+                String res = new String(new byte[]{(byte)read}, "UTF-8");
+                System.out.print(res);
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    @Ignore
+    public void testInputStreamWrapperText(TestContext context) {
+        Async async = context.async();
+        vertx.fileSystem().openObservable("static/test.txt", new OpenOptions().setWrite(true).setRead(true))
+                .flatMap(asyncFile -> {
+                    InputStream in = ReadInputStream.getInstance(asyncFile);//.start();
+                    FileOutputStream fileStr = null;
+
+                    try {
+                        int count = 0;
+
+                        fileStr = new FileOutputStream(new File("/tmp/testFile.txt"));
+                        int read = 0;
+                        byte[] bytes = new byte[1];
+
+                        while ((read = in.read(bytes)) != -1) {
+                            count++;
+                            fileStr.write(bytes, 0, read);
+                        }
+
+                        System.out.println("Done!");
+                        return Observable.just(null);
+                    } catch (IOException e) {
+                        return Observable.error(e);
+                    } finally {
+                        if (in != null) {
+                            try {
+                                in.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                        if (fileStr != null) {
+                            try {
+                                // outputStream.flush();
+                                fileStr.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    }
+                })
+                .subscribe(v -> {
+//                            File file = new File("/tmp/testFile.txt");
+//                            context.assertTrue(file.exists());
+                            async.complete();
+                        },
+                        context::fail);
     }
 }
